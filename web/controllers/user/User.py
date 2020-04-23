b2bcafcb2c60b1b5f4a6a40f91dfe441d678a57b
yuanbin1111 <1289340604@qq.com>
2020-04-23 13:34:15 +0800

    login页面

--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -1,7 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 router_user = Blueprint('user_page',__name__)
 
 @router_user.route('/login')
 def login():
-    return '登录页面'
+    return render_template('user/login.html')
+
+@router_user.route('/logout')
+def logout():
+    return '登出页面'
+
+@router_user.route('/edit')
+def edit():
+    return '编辑页面'
+
+@router_user.route('/reset-pwd')
+def resetPwd():
+    return '重置密码页面'
