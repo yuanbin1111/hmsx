e9746a24bb71186400a59cf5630152c545f365f2
yuanbin1111 <1289340604@qq.com>
2020-04-25 22:23:09 +0800

    首页路由，页面加载，静态资源加载

---
+++ web/controllers/index.py
@@ -0,0 +1,8 @@
+from application import app
+from flask import Blueprint,render_template
+
+router_index = Blueprint("index_page",__name__)
+
+@router_index.route('/')
+def index():
+    return render_template("index/index.html")
